31a70decfd5156416aeac061d901defd4969e921
Samuel Senesie <samuelsenesie@gmail.com>
2024-08-12 21:21:22 +0000

    Add code to make division answers a whole number

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,9 +85,9 @@ function calculateCorrectAnswer() {
     } else if (operator ==="x") {
         return[operand1 * operand2, "multiply"];
     } else if (operator === "-") {
-        return[operand1 - operand2, "subtract"]
+        return[operand1 - operand2, "subtract"];
     } else if (operator === "/") {
-        return[operand1 / operand2, "division"]
+        return[operand1 / operand2, "division"];
     } else {
         alert(`unimplemented operator ${operator}`);
         throw `unimplemented operator ${operator}. Aborting!`;
@@ -136,9 +136,10 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-function displayDivisionQuestion(operand1, operand2){
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "/";
 
 }
